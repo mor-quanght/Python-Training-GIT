26b9bf8eb2faf98c1fcbe2295ac886cc099e51ab
Hoang Lam <lamht@mor.com.vn>
2023-10-10 16:45:27 +0700

    them url, tao empty views

--- menu/urls.py
+++ menu/urls.py
@@ -7,5 +7,10 @@ from menu import views
 
 urlpatterns = [
     path('', views.my_menu,name='menu'),
-
+    path('them_sinh_vien',views.them_sinh_vien,name='them_sinh_vien'),
+    path('cap_nhap_bang_id', views.cap_nhap_bang_id, name='cap_nhap_bang_id'),
+    path('xoa_bang_id', views.xoa_bang_id, name='xoa_bang_id'),
+    path('tim_kiem_bang_ten', views.tim_kiem_bang_ten, name='tim_kiem_bang_ten'),
+    path('sap_xep', views.sap_xep, name='sap_xep'),
+    path('hien_thi', views.hien_thi, name='hien_thi'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
-def my_menu():
-    return 'menu'
+def my_menu(request):
+    return render(request,'menu.html')
+
+def them_sinh_vien(reqest):
+    pass
+
+def cap_nhap_bang_id(reqest):
+    pass
+
+def xoa_bang_id(reqest):
+    pass
+
+def tim_kiem_bang_ten(reqest):
+    pass
+
+def sap_xep(reqest):
+    pass
+
+def hien_thi(reqest):
+    pass
