8a5d412e51bf521c54b300be3a7200503680c084
Hoang Lam <lamht@mor.com.vn>
2023-10-12 11:02:20 +0700

    sua lai bai 5

--- bai_5.py
+++ bai_5.py
@@ -1,19 +1,27 @@
-#Bai 5. Viết chương trình Python nhập một dãy số nguyên, sau đó kiểm tra xem nó có khả đối xứng không? Nếu có, hãy biến đổi nó để được một dãy đối xứng.
+#Bai 5. Viết chương trình Python nhập một dãy số nguyên, sau đó kiểm tra xem nó có khả đối xứng không?
+# Nếu có, hãy biến đổi nó để được một dãy đối xứng.
 
-def doi_xung(day_so):
-        to_string=str(day_so)
-        if to_string==to_string[::-1]:
-            print(day_so)
-        else:
-            to_list=list(to_string)
-            id=0
-            while id<len(to_string)//2:
-                to_list[-(id+1)]=to_list[id]
-                id+=1
-            out_put= ''.join(to_list)
+def doi_xung():
+    day_so=int(input('Nhap day so:'))
+    set_left=set()
+    set_right=set()
+    my_list=list(str(day_so))
+    id=0
 
-            return int(out_put)
+    while id < len(my_list)//2:
+        set_left.add(my_list[id])
+        set_right.add(my_list[-(id+1)])
+        id+=1
+    if set_left-set_right!=set():
+        return 'Khong co kha nang doi xung'
+    else:
+        id = len(my_list)-1
 
-day_so=1234
+        while id>len(my_list)//2:
+            my_list[id]=set_left.pop()
+            id-=1
+        output = ''.join(my_list)
+        return output
 
-print(doi_xung(day_so))
+print(doi_xung())
+
