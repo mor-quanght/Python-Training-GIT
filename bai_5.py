54957d24aee1e7552bd4f3d85dc184633f814783
Hoang Lam <lamht@mor.com.vn>
2023-10-12 11:39:26 +0700

    ket hop vao 1 loop

--- bai_5.py
+++ bai_5.py
@@ -11,17 +11,13 @@ def doi_xung():
     while id < len(my_list)//2:
         set_left.add(my_list[id])
         set_right.add(my_list[-(id+1)])
-        id+=1
+        my_list[-(id + 1)] = my_list[id]
+        id += 1
 
     if set_left-set_right!=set():
         return 'Khong co kha nang doi xung'
     else:
-        id=0
-        while id < len(my_list)//2:
-            my_list[-(id+1)]=my_list[id]
-            id+=1
         output = ''.join(my_list)
-
         return output
 
 
